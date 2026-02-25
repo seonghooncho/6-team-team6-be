9bae68baa92a28f20153cff9330439252b19937b
hoooonshub <yhn515@naver.com>
2026-02-25 17:18:48 +0900

    log: fcm 초기화 확인 로그 추가

--- ktb-billage-infra/src/main/java/ktb/billage/infra/fcm/FcmConfig.java
+++ ktb-billage-infra/src/main/java/ktb/billage/infra/fcm/FcmConfig.java
@@ -4,16 +4,17 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Base64;
 
+@Slf4j
 @Configuration
 @ConditionalOnProperty(name = {"fcm.json_base64", "fcm.project_id"})
 public class FcmConfig {
@@ -26,16 +27,19 @@ public class FcmConfig {
     ) {
         this.firebaseResource = Base64.getDecoder().decode(firebaseKey);
         this.projectId = projectId;
+        log.info("FCM config loaded. projectId={}, jsonBytes={}", projectId, this.firebaseResource.length);
     }
 
     @Bean
     public FirebaseMessaging firebaseMessaging() throws IOException {
+        log.info("Creating FirebaseMessaging bean. projectId={}", projectId);
         return FirebaseMessaging.getInstance(firebaseApp());
     }
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         try {
+            log.info("Using existing FirebaseApp instance. projectId={}", projectId);
             return FirebaseApp.getInstance();
         } catch (IllegalStateException ignored) {
             ByteArrayInputStream in = new ByteArrayInputStream(firebaseResource);
@@ -45,6 +49,7 @@ public class FcmConfig {
                     .setCredentials(credentials)
                     .setProjectId(projectId)
                     .build();
+            log.info("Initializing new FirebaseApp instance. projectId={}", projectId);
             return FirebaseApp.initializeApp(options);
         }
     }
